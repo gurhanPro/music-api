e8084e0d9c1a4d6c1ff5da74b5ef7c72811ed196
Ziyaad Ahmed Gurhan <ziyaadyare@gmail.com>
2022-09-20 01:14:51 +0200

    connect to db

--- src/index.module.ts
+++ src/index.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common';
-
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import * as redisStore from 'cache-manager-redis-store';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { AppConfigs } from './configs/appConfigs';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    TypegooseModule.forRoot(AppConfigs.databaseURL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    }),
     
+    AuthModule
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor
+    }
   ]
 })
 export class AppModule {}
